af4be9c9ec9ff4824ebdc91be9637e45bf30f724
AlexRH <alexrupom@hotmail.com>
2019-09-29 02:29:42 +0600

    :nail_care:

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React from "react";
 import Campaign from "./components/Campaign";
 import "./App.scss";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-datepicker/dist/react-datepicker.css";
 
 class App extends React.Component {
   render() {
     return (
-    <div className="App">
-      <Campaign  />
-    </div>
-    )
+      <div className="App">
+        <Campaign />
+      </div>
+    );
   }
 }
-
 
 export default App;
